fa0484259496a896e6b56293fed4c1c116b22063
Victor Pasenchuk <victor.pasenchuk@aldigit.com>
2014-08-01 00:59:52 +0700

    Create package for main screen

--- app/src/main/java/ucsoftworks/com/bikestation/main_screen/MainActivity.java
+++ app/src/main/java/ucsoftworks/com/bikestation/main_screen/MainActivity.java
@@ -1,4 +1,4 @@
-package ucsoftworks.com.bikestation;
+package ucsoftworks.com.bikestation.main_screen;
 
 import android.app.Activity;
 import android.app.ActionBar;
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+import ucsoftworks.com.bikestation.R;
 
 
 public class MainActivity extends Activity {
