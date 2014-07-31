92aad2285f82fe1d78bef94a999b8eb74fd11984
Victor Pasenchuk <victor.pasenchuk@aldigit.com>
2014-08-01 00:57:25 +0700

    Add web service interface

--- app/src/main/java/ucsoftworks/com/bikestation/BikeApp.java
+++ app/src/main/java/ucsoftworks/com/bikestation/BikeApp.java
@@ -3,7 +3,7 @@ package ucsoftworks.com.bikestation;
 import android.app.Application;
 
 /**
- * Created by pasencukviktor on 31.07.14
+ * Created by Pasenchuk Victor on 31.07.14
  */
 public class BikeApp extends Application {
 }
---
+++ app/src/main/java/ucsoftworks/com/bikestation/web_service/BikeServiceApi.java
@@ -0,0 +1,8 @@
+package ucsoftworks.com.bikestation.web_service;
+
+/**
+ * Created by Pasenchuk Victor on 01.08.14
+ */
+public interface BikeServiceApi {
+
+}
