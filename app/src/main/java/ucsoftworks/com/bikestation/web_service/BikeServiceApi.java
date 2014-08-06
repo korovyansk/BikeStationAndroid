754e233678a1c3098afe077910d3b14024ac3c53
Victor Pasenchuk <victor.pasenchuk@aldigit.com>
2014-08-06 15:54:11 +0700

    Remove redundant methods.

--- app/src/main/java/ucsoftworks/com/bikestation/web_service/BikeServiceApi.java
+++ app/src/main/java/ucsoftworks/com/bikestation/web_service/BikeServiceApi.java
@@ -4,5 +4,4 @@ package ucsoftworks.com.bikestation.web_service;
  * Created by Pasenchuk Victor on 01.08.14
  */
 public interface BikeServiceApi {
-    public  BikeStatus getBikeStatus();
 }
--- app/src/main/java/ucsoftworks/com/bikestation/web_service/MockBikeApi.java
+++ app/src/main/java/ucsoftworks/com/bikestation/web_service/MockBikeApi.java
@@ -4,8 +4,4 @@ package ucsoftworks.com.bikestation.web_service;
  * Created by Pasenchuk Victor on 01.08.14
  */
 public class MockBikeApi implements BikeServiceApi {
-    @Override
-    public BikeStatus getBikeStatus() {
-        return new BikeStatus(false, null, null);
-    }
 }
