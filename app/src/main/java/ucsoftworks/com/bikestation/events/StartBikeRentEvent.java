92f4e565977ba9adfbc5752cc649f625380b93ec
Victor Pasenchuk <victor.pasenchuk@aldigit.com>
2014-08-06 15:51:35 +0700

    Filling Start Rent Event

--- app/src/main/java/ucsoftworks/com/bikestation/events/StartBikeRentEvent.java
+++ app/src/main/java/ucsoftworks/com/bikestation/events/StartBikeRentEvent.java
@@ -1,7 +1,31 @@
 package ucsoftworks.com.bikestation.events;
 
+import java.util.Date;
+
 /**
  * Created by Pasenchuk Victor on 06.08.14
  */
 public class StartBikeRentEvent {
+
+    private String username;
+    private Date rentDate;
+    private float cost;
+
+    public StartBikeRentEvent(String username, Date rentDate, float cost) {
+        this.username = username;
+        this.rentDate = rentDate;
+        this.cost = cost;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Date getRentDate() {
+        return rentDate;
+    }
+
+    public float getCost() {
+        return cost;
+    }
 }
