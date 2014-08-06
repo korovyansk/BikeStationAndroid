ca176f598616bd21f4efdd8211c1e83f25d999cd
Victor Pasenchuk <victor.pasenchuk@aldigit.com>
2014-08-06 15:59:39 +0700

    Filling stop rent event.

--- app/src/main/java/ucsoftworks/com/bikestation/events/StopBikeRentEvent.java
+++ app/src/main/java/ucsoftworks/com/bikestation/events/StopBikeRentEvent.java
@@ -1,7 +1,25 @@
 package ucsoftworks.com.bikestation.events;
 
+import java.util.Date;
+
 /**
  * Created by Pasenchuk Victor on 06.08.14
  */
+
 public class StopBikeRentEvent {
+    Date stopRentDate;
+    float totalCost;
+
+    public StopBikeRentEvent(Date stopRentDate, float totalCost) {
+        this.stopRentDate = stopRentDate;
+        this.totalCost = totalCost;
+    }
+
+    public Date getStopRentDate() {
+        return stopRentDate;
+    }
+
+    public float getTotalCost() {
+        return totalCost;
+    }
 }
