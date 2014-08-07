e186330c523e8f408519de5d58d7b1479952548e
Victor Pasenchuk <victor.pasenchuk@aldigit.com>
2014-08-07 14:46:29 +0700

    Add new provide methods to modules.

--- app/src/main/java/ucsoftworks/com/bikestation/modules/AppModule.java
+++ app/src/main/java/ucsoftworks/com/bikestation/modules/AppModule.java
@@ -1,12 +1,16 @@
 package ucsoftworks.com.bikestation.modules;
 
 import com.squareup.otto.Bus;
+import com.squareup.otto.ThreadEnforcer;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import ucsoftworks.com.bikestation.application.BikeApp;
+import ucsoftworks.com.bikestation.geolocation.GeoLocator;
+import ucsoftworks.com.bikestation.geolocation.GeolocationService;
+import ucsoftworks.com.bikestation.geolocation.MockGeoLocator;
 import ucsoftworks.com.bikestation.main_screen.MainActivity;
 import ucsoftworks.com.bikestation.web_service.BikeApi;
 import ucsoftworks.com.bikestation.web_service.BikeServiceApi;
@@ -15,7 +19,7 @@ import ucsoftworks.com.bikestation.web_service.BikeServiceApi;
  * Created by Pasenchuk Victor on 28.07.14 in IntelliJ Idea
  */
 
-@Module(injects = {BikeApp.class, MainActivity.class})
+@Module(injects = {BikeApp.class, MainActivity.class, MockGeoLocator.class}, library = true)
 public class AppModule {
     @Provides
     @Singleton
@@ -27,7 +31,12 @@ public class AppModule {
     @Provides
     @Singleton
     Bus provideBus() {
-        return new Bus();
+        return new Bus(ThreadEnforcer.ANY);
+    }
+
+    @Provides
+    GeolocationService provideGeolocationService() {
+        return new GeoLocator();
     }
 
 }
--- app/src/main/java/ucsoftworks/com/bikestation/modules/MockAppModule.java
+++ app/src/main/java/ucsoftworks/com/bikestation/modules/MockAppModule.java
@@ -4,6 +4,9 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import ucsoftworks.com.bikestation.application.BikeApp;
+import ucsoftworks.com.bikestation.geolocation.GeolocationService;
+import ucsoftworks.com.bikestation.geolocation.MockGeoLocator;
 import ucsoftworks.com.bikestation.web_service.BikeServiceApi;
 import ucsoftworks.com.bikestation.web_service.MockBikeApi;
 
@@ -17,9 +20,20 @@ import ucsoftworks.com.bikestation.web_service.MockBikeApi;
         overrides = true
 )
 public class MockAppModule {
+    BikeApp bikeApp;
+
+    public MockAppModule(BikeApp bikeApp) {
+        this.bikeApp = bikeApp;
+    }
+
     @Provides
     @Singleton
     BikeServiceApi provideBikeService() {
         return new MockBikeApi();
+    }
+
+    @Provides
+    GeolocationService provideGeolocationService() {
+        return new MockGeoLocator(bikeApp);
     }
 }
