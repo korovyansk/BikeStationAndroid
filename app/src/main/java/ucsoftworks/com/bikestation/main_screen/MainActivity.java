3a4b800039cd8978e5ebf14185497ac4ebbe2b3e
Victor Pasenchuk <victor.pasenchuk@aldigit.com>
2014-08-11 15:43:41 +0700

    Hide action bar.

--- app/src/main/java/ucsoftworks/com/bikestation/main_screen/MainActivity.java
+++ app/src/main/java/ucsoftworks/com/bikestation/main_screen/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().hide();
         setContentView(R.layout.activity_main);
 
         BikeApp bikeApp = (BikeApp) getApplication();
