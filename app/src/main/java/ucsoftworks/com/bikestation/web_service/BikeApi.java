8c4233bdb40f4ac5615ad0738e6be0ea91d42d86
Victor Pasenchuk <victor.pasenchuk@aldigit.com>
2014-08-14 16:20:51 +0700

    Started to write api with retrofit.

--- app/src/main/java/ucsoftworks/com/bikestation/web_service/BikeApi.java
+++ app/src/main/java/ucsoftworks/com/bikestation/web_service/BikeApi.java
@@ -1,7 +1,18 @@
 package ucsoftworks.com.bikestation.web_service;
 
+import retrofit.http.Path;
+
 /**
  * Created by Pasenchuk Victor on 01.08.14
  */
 public class BikeApi implements BikeServiceApi {
+    @Override
+    public int registerBike() {
+        return 0;
+    }
+
+    @Override
+    public int sendLocation(@Path("uuid") String uuid) {
+        return 0;
+    }
 }
